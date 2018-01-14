1018955f8fcc3685767be50cfd9eae7d7848e74f
luan <luanharaujo@gmail.com>
2018-01-14 10:56:21 -0200

    acressentando a pasta como script e as bibliotecas para compilação crusada

---
+++ ccdir/compile_rasp.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+if [ $# -lt 2 ]; then
+	echo "ERROR: Falta argumentos, parça!"
+	echo "Arg1 = Nome do Arquivo C"
+	echo "Arg2 = Nome do Arquivo Objeto de Saída"
+	echo "Arg3 = IP do Raspberry"
+else
+	if [ $# -lt 3 ]; then
+		echo "Warning: IP Address as default (192.168.200.1)!"
+		ip_rasp="192.168.200.1"
+	else
+		ip_rasp=${3}
+	fi
+	
+	nome_codigo=${1}
+	nome_binario=${2}
+
+	
+	login_rasp="pi"
+	#password="godroidgo"
+	dst_folder_rasp="/home/pi/ccdir"
+
+	arm-linux-gnueabi-gcc -o ${nome_binario} ${nome_codigo} -I. -L. ./libwiringPi.so
+
+	# encrypted_password="$(perl -e 'printf("%s\n", crypt($ARGV[0], "password"))' "${password}")"
+
+	scp ./${nome_binario} ${login_rasp}@${ip_rasp}:${dst_folder_rasp}
+	
+fi
+
