403692940ccfd347a3e6c4356a9895c6802ac06c
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-01-16 00:12:42 -0200

    Added su check to main.c and set up wiringPi in Phys mode

--- codigos/main.c
+++ codigos/main.c
@@ -19,6 +19,16 @@ PI_THREAD(joystick)
 
 int main()
 {
+	char resposta;
+    do
+    {
+    	printf("Are you su?(y/n)\n");
+   		scanf("%c", &resposta);
+    } while(resposta != 'y' && resposta != 'n');
+    if(resposta!='y')
+    	return 0;
+
+    wiringPiSetupPhys();
 	piThreadCreate(joystick);
 	
 	while(getchar() != '\n');
