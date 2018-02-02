43cb51f10dec006b8aee46a9993f538592b4cc66
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-02-02 21:07:29 -0200

    Edited compile_test.sh to use new compiler

--- code/compile_test.sh
+++ code/compile_test.sh
@@ -6,8 +6,8 @@ else
 	nome_codigo=${1}
 	nome_binario=compilation_test
 
-	arm-linux-gnueabi-gcc -o ${nome_binario} ${nome_codigo} -I. -L. ./libwiringPi.so -lm
-
+	../gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/arm-linux-gnueabihf-gcc -o ${nome_binario} ${nome_codigo} -I. -L. ./libwiringPi.so -lm
+	
 	# encrypted_password="$(perl -e 'printf("%s\n", crypt($ARGV[0], "password"))' "${password}")"
 
 	# scp ./${nome_binario} ${login_rasp}@${ip_rasp}:${dst_folder_rasp}
