eee52d5ba6629000ef60a2cfaf887ef648ca85cf
Luan Haickel Araújo <luanharaujo@gmail.com>
2018-02-08 21:05:38 -0200

    regulando os valores recebidos pelo matlab

--- MATLAB/client.m
+++ MATLAB/client.m
@@ -3,27 +3,23 @@ fopen(t);%abrindo o  canal de comunicação
 
 while true
     
-    data = fread(t, 100);%recebendo os dados 
+    data = fread(t, 102);%recebendo os dados 
     fwrite(t, "ok!");%mandano a confirmação para o server
     tdata = char(data');%colocando os caracteres recebidos em vetor linha
     
     %convertendo de char para floats
     datanum = regexp(tdata,'[+-]?\d+\.?\d*', 'match');
     
-    acellX = str2num(datanum{1});
-    acellY = str2num(datanum{2});
-    acellZ = str2num(datanum{3});
-    magX = str2num(datanum{4});
-    magY = str2num(datanum{5});
-    magZ = str2num(datanum{6});
-    gyroX = str2num(datanum{7})
-    gyroY = str2num(datanum{8})
-    gyroZ = str2num(datanum{9})
-    dt = str2num(datanum{10})
-    
-    
-    
-    
+    gyroX = str2num(datanum{1});
+    gyroY = str2num(datanum{2});
+    gyroZ = str2num(datanum{3});
+    acellX = str2num(datanum{4});
+    acellY = str2num(datanum{5});
+    acellZ = str2num(datanum{6});
+    magX = str2num(datanum{7});
+    magY = str2num(datanum{8});
+    magZ = str2num(datanum{9});
+    dt = str2num(datanum{10});
     
     pause(0.05)
 end
