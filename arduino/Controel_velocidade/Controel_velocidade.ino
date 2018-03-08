9d36b123f923d83be5434f6b7f0ea42c864963e3
Luan Haickel Araújo <luanharaujo@gmail.com>
2018-03-08 20:44:22 -0300

    implementação do controle PID para contnrole do motor pelo arduino e adaptação do controle da letícia para o arduino nano

--- arduino/Controel_velocidade/Controel_velocidade.ino
+++ arduino/Controel_velocidade/Controel_velocidade.ino
@@ -4,6 +4,11 @@
  *cdigo da leticia que fizemos para a open 2017
  */
 
+#define DEBUG 1//1 - ligado /// 0 - desligado
+
+#define PI 3.14159265359
+#define RAIO 0.100 // 100mm
+
 //driver
 #define PWM_L 9
 #define INA_L 4
@@ -14,14 +19,188 @@
 #define INB_R 7
 
 //encoder
-#define encoderL 2
-#define encoderR 3
-#define dirL 11
-#define	dirR 12
+#define ENCODER_L 0 //interrup port 0, is the pin 2
+#define ENCODER_R 1 //interrup port 1, is the pin 3
+#define DIR_L 11
+#define  DIR_R 12
 
 
+//variaveis
 volatile long encoder_posL = 0; 
 volatile long encoder_posR = 0;
+
+unsigned long tempo_aux;
+unsigned long tempo;
+float voltas_esquerda;
+float voltas_direita;
+float velocidade_esquerda;
+float velocidade_direita;
+float voltas_esquerda_anterior;
+float voltas_direita_anterior;
+float dt;
+
+//Declaração das funções
+void startDriver();
+void startEncoder();
+void interrupt_L();
+void interrupt_R();
+void UpdateVel(float refL,float refR);
+void setpot(int potL, int potR);
+void controle(float refL, float refR);
+
+
+
+//funões principais
+void setup()
+{
+        noInterrupts();
+        
+        startDriver();
+        startEncoder();
+        if(DEBUG)
+         Serial.begin(9600);
+        
+        interrupts();
+}
+
+float ref = 0.2;
+
+int flag = 0;
+unsigned long int tempo_loop = 0;
+void loop()
+{
+  if(tempo_loop - millis() > 1000)
+    if(flag)
+      flag = 0;
+    else
+      flag = 1;
+  if(flag)  
+    UpdateVel(0,ref);
+  else
+    UpdateVel(0,-ref)
+
+  if(DEBUG)
+  {  
+     Serial.print(ref);
+     Serial.print("\t");
+     Serial.print(velocidade_direita);
+     Serial.print("\t");
+     Serial.println(velocidade_esquerda);
+  }
+  //delay(1);
+}
+
+#define KP 60
+#define KI 200
+#define KD 800
+
+float errL = 0, sum_errL = 0, old_errL, derrL;
+float errR = 0, sum_errR = 0, old_errR, derrR; 
+
+int pwmL, pwmR;
+
+void controle(float refL, float refR)
+{
+  old_errL = errL;
+  old_errR = errR;
+  errL = refL - velocidade_esquerda;
+  errR = refR - velocidade_direita;
+
+  derrR = (errR - old_errR)*dt;
+  derrL = (errL - old_errL)*dt;
+
+  sum_errR += errR*dt;
+  sum_errL += errL*dt;
+
+  pwmR = (int)(KP*errR + KI*sum_errR + KD*derrR);
+  pwmL = (int)(KP*errL + KI*sum_errL + KD*derrL);
+
+  setpot(pwmL,pwmR);
+}
+
+void setpot(int potL, int potR)
+{
+  if(potL > 0)
+  {   
+    if(potL > 255)
+      potL = 255;
+
+    digitalWrite(INA_L, LOW);
+    digitalWrite(INB_L, HIGH);
+    analogWrite(PWM_L, potL);
+  }
+  else if(potL < 0)
+  {
+    potL = -potL;
+    if(potL > 255)
+      potL = 255;
+
+    digitalWrite(INA_L, HIGH);
+    digitalWrite(INB_L, LOW);
+    analogWrite(PWM_L, potL);
+  }
+  else //potL == 0
+  {
+    digitalWrite(INA_L, HIGH);
+    digitalWrite(INB_L, HIGH);
+    analogWrite(PWM_L, 0);
+  }
+
+  if(potR > 0)
+  {   
+    if(potR > 255)
+      potR = 255;
+
+    digitalWrite(INA_R, LOW);
+    digitalWrite(INB_R, HIGH);
+    analogWrite(PWM_R, potR);
+  }
+  else if(potR < 0)
+  {
+    potR = -potR;
+    if(potR > 255)
+      potR = 255;
+
+    digitalWrite(INA_R, HIGH);
+    digitalWrite(INB_R, LOW);
+    analogWrite(PWM_R, potR);
+  }
+  else //potL == 0
+  {
+    digitalWrite(INA_R, HIGH);
+    digitalWrite(INB_R, HIGH);
+    analogWrite(PWM_R, 0);
+  }
+
+}
+
+//assinatura das funnções
+void UpdateVel(float refL,float refR) {
+   // Calcula as velocidades das rodas a cada 20 ms ---------------------------------
+   if (millis() - tempo > 20) 
+   {      
+      tempo_aux = (millis() - tempo);
+      tempo = millis();
+
+      dt = ((float)tempo_aux)/1000.0;
+
+      voltas_esquerda = encoder_posL / (800.0); // 400 pontos por volta só que pegando a subida e a descida
+      voltas_direita = encoder_posR / (800.0); 
+
+      //velocidade angular em revoluções/segundo
+      velocidade_esquerda = 2*PI*RAIO * 1000 * (voltas_esquerda - voltas_esquerda_anterior) / (tempo_aux);//o *1000 é para corrigir a unidade de tempo
+      velocidade_direita  = 2*PI*RAIO * 1000 * (voltas_direita - voltas_direita_anterior) / (tempo_aux);
+      
+      voltas_esquerda_anterior = voltas_esquerda;
+      voltas_direita_anterior = voltas_direita;
+      // if(DEBUG)
+      //   Serial.println(tempo_aux);
+      controle(refL,refR);
+  }
+  // Chama a funcao que calcula a tensao de saida para os motores -----------------------
+  //controleAdaptativoVelocidade();
+}
+
 
 void startDriver() {
   pinMode(INA_L, OUTPUT);
@@ -33,56 +212,28 @@ void startDriver() {
 }
 
 void startEncoder () {
-  pinMode(encoderL, INPUT);
-  pinMode(encoderR, INPUT);
+  pinMode(ENCODER_L, INPUT);
+  pinMode(ENCODER_R, INPUT);
 
-  attachInterrupt(0, interrupt_L, CHANGE);
-  attachInterrupt(1, interrupt_R, CHANGE);
+  attachInterrupt(ENCODER_L, interrupt_L, CHANGE);
+  attachInterrupt(ENCODER_R, interrupt_R, CHANGE);
 }
 
 
 void interrupt_L() {
   noInterrupts();
-  if(digitalRead(dirL))
-  	encoder_posL++;
+  if(digitalRead(DIR_L))
+    encoder_posL--;
   else
-  	encoder_posL--;
+    encoder_posL++;
   interrupts();
 }
 
 void interrupt_R() {
   noInterrupts();
-  if(digitalRead(dirR))
-  	encoder_posR++;
+  if(digitalRead(DIR_R))
+    encoder_posR++;
   else
-  	encoder_posR--;
+    encoder_posR--;
   interrupts();
-}
-
-void setup()
-{
-      	noInterrupts();
-      	
-      	startDriver();
-      	startEncoder();
-      	Serial.begin(9600);
-      	
-      	interrupts();
-}
-
-void loop()
-{
-    digitalWrite(INA_L, HIGH);
-    digitalWrite(INB_L, LOW);
-    digitalWrite(PWM_L, 100);
-    digitalWrite(INA_R, HIGH);
-    digitalWrite(INB_R, LOW);
-    digitalWrite(PWM_R, 100);
-    
-    Serial.print(encoder_posL);
-    Serial.print("\t");
-    Serial.println(encoder_posR);
-    
-    delay(100);
- 
 }
